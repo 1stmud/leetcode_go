2c4492cebd55b67549e2620540494c31e729131b
wudahai <564818745@qq.com>
2020-03-05 19:08:02 +0800

    remove element

--- debug/debug_test.go
+++ debug/debug_test.go
@@ -7,8 +7,27 @@ import (
 
 func Test_main(t *testing.T) {
 	nums := []int{1, 1, 2, 3, 3}
-	a := removeDuplicates(nums)
+	d := 2
+	a := removeElement(nums, d)
 	fmt.Println("ssss", a)
+}
+
+func removeElement(nums []int, val int) int {
+
+	if len(nums) == 0 {
+		return 0
+	}
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums = append(nums[i], nums[j])
+			i++
+		}
+	}
+
+	fmt.Print(nums)
+	fmt.Print(i)
+	return i
 }
 
 func removeDuplicates(nums []int) int {
